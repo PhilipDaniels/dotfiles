5744d006b0493564356aeff998e77345d7d29cee
Philip Daniels <Philip.Daniels1971@gmail.com>
2015-02-27 16:30:49 +0000

    Add code snippets and installation script.

---
+++ Visual Studio/install_snippets.ps1
@@ -0,0 +1,24 @@
+﻿$scriptDir = Split-Path $MyInvocation.MyCommand.Path -Parent
+
+foreach ($vsVer in 2012,2013,2015)
+{
+    $sourceDir = "$scriptDir\Code Snippets"
+    $snippetDir = "{0}\My Documents\Visual Studio {1}\Code Snippets" -f $env:UserProfile, $vsVer
+    #$snippetDir = "C:\temp\tst"
+
+    if (Test-Path $snippetDir)
+    {
+        foreach ($srcFile in Get-ChildItem $sourceDir -Recurse -Filter "*.snippet")
+        {
+            $dstFile = Join-Path $snippetDir $srcFile.FullName.Substring($sourceDir.Length)
+            $dstDir = Split-Path $dstFile -Parent
+            New-Item $dstDir -Type Directory -Force > $null
+            Copy-Item -Force -Path $srcFile.FullName -Destination $dstFile
+            Write-Host "Copied snippet to $dstFile"
+        }
+    }
+    else
+    {
+        Write-Host "$snippetDir DOES NOT EXIST - snippets not copied."
+    }
+}
