de2226161bb2cd07f16c3093503199ef54c0d1f3
Philip Daniels <Philip.Daniels1971@gmail.com>
2015-03-20 12:30:43 +0000

    Add clean_snippets.ps1 script.

---
+++ Visual Studio/clean_snippets.ps1
@@ -0,0 +1,21 @@
+﻿# For Visual Studio, these are the folders where snippets are stored.
+# Default snippets : C:\Program Files (x86)\Microsoft Visual Studio {0}\LANGUAGE\Snippets   (varies by VS version, SQL_SSDT affects SQL files)
+# User snippets    : %MYDOCUMENTS%\Visual Studio {0}\Code Snippets                          (varies by VS version)
+
+
+$myDocs = [Environment]::GetFolderPath("mydocuments")  # Differs on Win7/8.
+
+# Clean Visual Studio snippets.
+foreach ($vsVer in 2012,2013,2015)
+{
+    $snippetDir = "{0}\Visual Studio {1}\Code Snippets" -f $myDocs, $vsVer
+
+    if (Test-Path $snippetDir)
+    {
+        foreach ($file in Get-ChildItem $snippetDir -Recurse -Filter "*.snippet")
+        {
+			Remove-Item $file.FullName
+			Write-Host ("Deleted snippet {0}" -f $file.FullName)
+        }
+    }
+}
