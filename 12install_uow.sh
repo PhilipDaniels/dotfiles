17ce5313164f15c0e7cd38e43025ae07289502c8
Philip Daniels <Philip.Daniels1971@gmail.com>
2016-04-20 21:48:42 +0000

    Update UoW script.

--- 12install_uow.sh
+++ 12install_uow.sh
@@ -17,13 +17,14 @@ bison \
 build-essential \
 checkinstall \
 colorgcc \
-        curl \
-        docbook \
-        docbook2x \
-        docbook-utils \
-        emacs \
-        exuberant-ctags \
-        fortune-mod \
+curl \
+docbook \
+docbook2x \
+docbook-utils \
+emacs \
+exuberant-ctags \
+fortune-mod \
+fortunes \
 flex \
 g++ \
 gcc \
@@ -31,40 +32,48 @@ gdb \
 gettext \
 gimp \
 git \
+kdiff3 \
 libc6-dev \
 libcurl4-openssl-dev \
+libdbus-1-dev \
 libgif-dev \
+libgtk2.0-dev \
 libice-dev \
-libjpeg62-dev \
+libjpeg-turbo8-dev \
+libm17n-dev \
 libncurses5-dev \
+libotf-dev \
 libpng12-dev \
+librsvg2-dev \
 libsm-dev \
-        libtiff4-dev \
-        libtool \
+libtiff4-dev \
+libtiff5-dev \
+libtool \
 libx11-dev \
 libxext-dev \
-        libxi-dev \
-        libxml2-dev \
+libxi-dev \
+libxml2-dev \
 libxmu-dev \
 libxmuu-dev \
 libxpm-dev \
 libxrandr-dev \
 libxt-dev \
 libxtst-dev \
-        libxv-dev \
+libxv-dev \
 m4 \
 make \
-        ncdu \
-        pandoc \
-        rsync \
-        screen \
-        strace \
-        tig \
-        tmux \
-        tree \
+ncdu \
+pandoc \
+rsync \
+screen \
+strace \
+texinfo \
+tig \
+tmux \
+tree \
 xaw3dg-dev \
 x11-apps \
 xfe \
-        xmlto \
-        xorg-dev \
+xmlto \
+xorg-dev \
 zlib1g-dev
