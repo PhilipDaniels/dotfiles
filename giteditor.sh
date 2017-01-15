16de65e10822fec24c2f09254a1b538df0adc30b
Philip Daniels <Philip.Daniels1971@gmail.com>
2017-01-15 11:25:03 +0000

    Run git editor via a script.

---
+++ giteditor.sh
@@ -0,0 +1,22 @@
+#!/bin/sh --noprofile
+
+# This script is run when doing a Git Commit to pick the editor.
+# http://emacs.stackexchange.com/questions/2417/open-edit-server-files-from-emacsclient-in-a-specific-frame?rq=1
+
+if [ "$WINDIR" == "" ]; then
+    # Running on Linux.
+    if [ "$INSIDE_EMACS" == "" ]; then
+        emacsclient -c $1;
+    else
+        emacsclient $1;
+    fi
+else
+    if [ "$INSIDE_EMACS" == "" ]; then
+        emacsclient-w32 -c $1;
+    else
+        emacsclient-w32 $1;
+    fi
+fi
+
+
+
