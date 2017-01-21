55d6fbd049972dcf0effd1da84a1a972ef867f9f
Philip Daniels <Philip.Daniels1971@gmail.com>
2017-01-21 23:26:43 +0000

    Make start/stop_emacs_daemon scripts quiet.

--- bin/start_emacs_daemon.sh
+++ bin/start_emacs_daemon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if type emacs-w32 > /dev/null; then
+if type emacs-w32 > /dev/null 2&>1; then
     emacs-w32 --daemon
 else
     emacs --daemon
--- bin/stop_emacs_daemon.sh
+++ bin/stop_emacs_daemon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if type emacs-w32 > /dev/null; then
+if type emacsclient-w32 > /dev/null 2>&1; then
     emacsclient-w32 -e '(kill-emacs)'
 else
     emacsclient -e '(kill-emacs)'
