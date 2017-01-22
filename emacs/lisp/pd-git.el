92b8c8184136a12db713912e21da4f4af992d375
Philip Daniels <Philip.Daniels1971@gmail.com>
2017-01-22 10:49:03 +0000

    Incorporate ssh-agency to try and stop Magit prompting for passwords.

--- emacs/lisp/pd-git.el
+++ emacs/lisp/pd-git.el
@@ -6,6 +6,7 @@
 (require-package 'gitattributes-mode)
 (require-package 'gitignore-mode)
 (require-package 'magit)
+(require-package 'ssh-agency)
 
 (require 'pd)
 
@@ -16,5 +17,23 @@
 (add-hook 'magit-diff-mode-hook 'pd-hide-dos-eol)
 (add-hook 'git-timemachine-mode-hook 'pd-hide-dos-eol)
 (add-hook 'magit-mode-popup-hook 'pd-turn-off-trailing-whitespace-display)
+
+;; Configure Magit so that it will either use my existing ssh-agent, or
+;; prompt for passwords if none is yet known. In Cygwin, this will require
+;; X to be started.
+;; From https://github.com/magit/magit/wiki/Pushing-with-Magit-from-Windows#openssh-passphrase-caching-via-ssh-agent
+
+(when (eq system-type 'cygwin)
+  (setq ssh-agency-add-executable "/bin/ssh-add.exe")
+  (setq ssh-agency-agent-executable "/bin/ssh-agent.exe"))
+
+;; Prompt for HTTPS passwords if not cached.
+(setenv "GIT_ASKPASS" "git-gui--askpass")
+
+;; Make ssh-ahency prompt for my ssh passphrase using a graphical prompt.
+;; This should in fact not be necessary, because ssh-agent should be
+;; started from my cygwin login script.
+(setenv "SSH_ASKPASS" "git-gui--askpass")
+
 
 (provide 'pd-git)
