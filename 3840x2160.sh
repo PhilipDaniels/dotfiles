ff6277c6a426bab749a9e5de541898d626363c77
Philip Daniels <Philip.Daniels1971@gmail.com>
2021-11-27 11:19:40 +0000

    Add scaling scripts

---
+++ 3840x2160.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --off --output HDMI-0 --off --output DP-0 --mode 3840x2160 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --primary --mode 3840x2160 --pos 3840x0 --rotate normal --output DP-3 --off --output DP-4 --mode 3840x2160 --pos 7680x0 --rotate normal --output DP-5 --off
