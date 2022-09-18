f028efda85bbfb34de4c268f11d949ba0be5c0a8
Philip Daniels <Philip.Daniels1971@gmail.com>
2022-09-18 16:40:30 +0100

    Fix dotfiles screen scripts for new DP cables

--- 2560x1440.sh
+++ 2560x1440.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-xrandr --output DVI-D-0 --off --output HDMI-0 --off --output DP-0 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --primary --mode 2560x1440 --pos 3840x0 --rotate normal --output DP-3 --off --output DP-4 --mode 2560x1440 --pos 7680x0 --rotate normal --output DP-5 --off
+xrandr --output DVI-D-0 --off \
+    --output HDMI-0 --off \
+    --output DP-0 --mode 2560x1440 --pos 5120x0 --rotate normal \
+    --output DP-1 --off \
+    --output DP-2 --primary --mode 2560x1440 --pos 2560x0 --rotate normal \
+    --output DP-3 --off \
+    --output DP-4 --mode 2560x1440 --pos 0x0 --rotate normal \
+    --output DP-5 --off
--- 3840x2160.sh
+++ 3840x2160.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-xrandr --output DVI-D-0 --off --output HDMI-0 --off --output DP-0 --mode 3840x2160 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --primary --mode 3840x2160 --pos 3840x0 --rotate normal --output DP-3 --off --output DP-4 --mode 3840x2160 --pos 7680x0 --rotate normal --output DP-5 --off
+xrandr --output DVI-D-0 --off \
+    --output HDMI-0 --off \
+    --output DP-0 --mode 3840x2160 --pos 7680x0 --rotate normal \
+    --output DP-1 --off \
+    --output DP-2 --primary --mode 3840x2160 --pos 3840x0 --rotate normal \
+    --output DP-3 --off \
+    --output DP-4 --mode 3840x2160 --pos 0x0 --rotate normal \
+    --output DP-5 --off
